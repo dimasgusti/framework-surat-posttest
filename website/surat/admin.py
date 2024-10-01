7857ea4433730e99e4cc47b70d03c6c0d436bbdb
dimasgusti <dimsgusticode@gmail.com>
2024-10-01 22:04:15 +0800

    Posttest 3

--- website/surat/admin.py
+++ website/surat/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
 from .models import Sender, Letter
 
 class SenderModel(admin.ModelAdmin):
     list_display = ('nama', 'email', 'no_hp', 'alamat')
     search_fields = ('nama', 'email')
     list_filter = ('no_hp',)
+    
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        if not change:
+            User.objects.create_user(
+                username=obj.email,
+                email=obj.email,
+                password='defaultpassword'
+            )
 
 class LetterModel(admin.ModelAdmin):
     list_display = ('judul', 'tanggal', 'pengirim')
