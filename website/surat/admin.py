567a2750a6d955833bc31767ad850141f864a411
dimasgusti <dimsgusticode@gmail.com>
2024-10-01 21:43:27 +0800

    Posttest 2

--- website/surat/admin.py
+++ website/surat/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.hashers import make_password
+from .models import Sender, Letter
 
-# Register your models here.
+class SenderModel(admin.ModelAdmin):
+    list_display = ('nama', 'email', 'no_hp', 'alamat')
+    search_fields = ('nama', 'email')
+    list_filter = ('no_hp',)
+
+class LetterModel(admin.ModelAdmin):
+    list_display = ('judul', 'tanggal', 'pengirim')
+    search_fields = ('judul', 'isi')
+    list_filter = ('tanggal', 'pengirim')
+
+admin.site.register(Sender, SenderModel)
+admin.site.register(Letter, LetterModel)
--- website/surat/models.py
+++ website/surat/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Sender(models.Model):
+    nama = models.CharField(max_length=100)
+    email = models.EmailField()
+    no_hp = models.CharField(max_length=15)
+    alamat = models.TextField()
+    
+    def __str__(self):
+        return self.nama
+    
+class Letter(models.Model):
+    judul = models.CharField(max_length=200)
+    pesan = models.TextField()
+    tanggal = models.DateField(default=datetime.now)
+    pengirim = models.ForeignKey(Sender, on_delete=models.CASCADE, related_name='surat')
+    
+    def __str__(self):
+        return self.judul
